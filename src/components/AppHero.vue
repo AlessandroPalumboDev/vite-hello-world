bc71083a3adc75dede9f85cde4d0e8a8d9bc2118
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-29 23:45:29 +0200

    [UPD] creati componenti e utilizzati nell applicativo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,24 @@
+<script>
+export default{
+    name: 'AppHero',
+    data(){
+        return{
+            sottotitolo:'JUMBOTRON',
+        };
+    },
+}
+</script>
+
+<template>
+<div>
+    <h2>{{ sottotitolo }}</h2>
+</div>
+
+</template>
+
+<style>
+h2{
+    color: green;
+    text-decoration: overline;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            title: 'Sono la mia prima App con Vite!'
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+
+</template>
+
+<style>
+h1{
+  color: blue;
+  text-decoration:underline;
+}
+</style>
